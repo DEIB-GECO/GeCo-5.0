df3068284c4278b52395288a60269391fbc05acd
Pietro <p.crovari@gmail.com>
2020-04-30 11:09:22 +0200

    pulizia Kontatty

--- frontend/src/components/toolbox/tools/data_visualization/charts/piechart.vue
+++ frontend/src/components/toolbox/tools/data_visualization/charts/piechart.vue
@@ -1,114 +1,108 @@
 <template>
   <div>
     <div id="piechart"></div>
-    <div id="whatever"></div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import { select } from "d3-selection";
-import { scaleOrdinal } from "d3-scale";
-import { pie, arc } from "d3-shape";
-import { entries } from "d3-collection";
-// import PieLayout from "D3";s
-// import {pie} as pppp from layout;
-import * as d3 from "D3";
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { select } from 'd3-selection';
+import { scaleOrdinal } from 'd3-scale';
+import { pie, arc, PieArcDatum } from 'd3-shape';
+import { entries } from 'd3-collection';
+
 interface PieData {
   label: string;
   value: number;
 }
 
-// const d3 = Object.assign(
-//   {},
-//   {
-//     select: Function,
-//   }
-// );
+const d3 = Object.assign(
+  {},
+  {
+    select,
+    scaleOrdinal,
+    pie,
+    arc,
+    entries
+  }
+);
 
-export default Vue.extend({
-  data() {
-    return {
-      width: 450,
-      height: 450,
-      margin: 40,
-    };
-  },
-  props: {
-    chartData: {
-      type: Array,
-      default: () => {
-        return [
-          { label: "primo", value: 4 },
-          { label: "secondo", value: 5 },
-          { label: "terzo", value: 2 },
-          { label: "quarto", value: 5 },
-        ];
-      },
-    },
-  },
-  methods: {
-    plotPie() {
-      // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-      const radius = Math.min(this.width, this.height) / 2 - this.margin;
+@Component
+export default class Piechart extends Vue {
+  @Prop({
+    default: () => [
+      { label: 'primo', value: 4 },
+      { label: 'secondo', value: 5 },
+      { label: 'terzo', value: 2 },
+      { label: 'quarto', value: 5 }
+    ]
+  })
+  chartData!: PieData[];
 
-      //   append the svg object to the div called 'my_dataviz'
-      const svg = select("#piechart")
-        .append("svg")
-        .attr("width", this.width)
-        .attr("height", this.height)
-        .append("g")
-        .attr(
-          "transform",
-          "translate(" + this.width / 2 + "," + this.height / 2 + ")"
-        );
+  width = 450;
+  height = 450;
+  margin = 40;
 
-      const dataArray: any = this.chartData.reduce(
-        (acc: any, item: any) => ((acc[item.label] = item.value), acc),
-        {}
-      );
-      console.log(dataArray);
+  plotPie() {
+    const data = this.chartData.map((x) => {
+      return {
+        label: x.label,
+        value: x.value
+      };
+    });
+    const radius = Math.min(this.width, this.height) / 2 - this.margin;
 
-      //FINO A QUA
+    const svg = d3.select<Element, PieData>('#piechart');
+    const g = svg
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .append('g')
+      .attr('transform', `translate(${this.width / 2}, ${this.height / 2})`);
 
-      // set the color scale
-      const color = scaleOrdinal()
-        .domain(dataArray)
-        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"]);
+    const color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3']);
+    const myPie = d3.pie<PieData>().value(function(d) {
+      return d.value;
+    });
 
-      // Compute the position of each group on the pie:
-      const myPie = pie().value(function(d: any) {
-        return d.value;
+    const path = d3
+      .arc<PieArcDatum<PieData>>()
+      .outerRadius(radius - 10)
+      .innerRadius(0);
+
+    const label = d3
+      .arc<PieArcDatum<PieData>>()
+      .outerRadius(radius)
+      .innerRadius(radius - 80);
+
+    const arc = g
+      .selectAll('.arc')
+      .data(myPie(data))
+      .enter()
+      .append('g')
+      .attr('class', 'arc');
+
+    arc
+      .append('path')
+      .attr('d', path)
+      .attr('fill', function(d) {
+        return color(d.data.label);
       });
-      // const dataReady = pie(entries(dataArray));
-      const dataReady = pie();
-      console.log(dataReady);
 
-      // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-      svg
-        .selectAll("whatever")
-        .data(dataReady)
-        .enter()
-        .append("path")
-        .attr(
-          "d",
-          arc<any>()
-            .innerRadius(0)
-            .outerRadius(radius)
-        )
-        .attr("fill", (d: any) => {
-          console.log(d);
-          return color(d.data.key);
-        })
-        .attr("stroke", "black")
-        .style("stroke-width", "2px")
-        .style("opacity", 0.7);
-    },
-  },
+    arc
+      .append('text')
+      .attr('transform', function(d) {
+        return `translate(${label.centroid(d)})`;
+      })
+      .text(function(d) {
+        return d.data.label;
+      });
+  }
+
   mounted() {
     this.plotPie();
-  },
-});
+  }
+}
 </script>
 
 <style scoped></style>
