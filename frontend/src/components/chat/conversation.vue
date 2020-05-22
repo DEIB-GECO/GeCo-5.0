c4f952c41cc200b004bd7b0d7089d694ff9779fb
Pietro <p.crovari@gmail.com>
2020-05-22 10:49:42 +0200

    Message Rifactored

--- frontend/src/components/chat/conversation.vue
+++ frontend/src/components/chat/conversation.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
     <div class="chat" id="chat">
-      <message
+      <div
         v-for="item in conversation"
         :key="item.id"
-        :text="item.text"
-        :sender="item.sender"
-      ></message>
+        :class="['chat_line', item.sender]"
+      >
+        <div class="icon_container">
+          <!-- <img src="./../../assets/logo.png" alt class="icon" /> -->
+          <font-awesome-icon
+            class="icon"
+            :icon="['fas', item.sender == 'user' ? 'user-astronaut' : 'robot']"
+            size="2x"
+          />
+        </div>
+        <div class="message_box">{{ item.text }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-// import Vue from 'vue';
-import Message from './message.vue';
-
 import { Component, Vue, Prop } from 'vue-property-decorator';
 import { namespace } from 'vuex-class';
 
 const conversationStore = namespace('gecoAgent/conversation');
 
-import { mapState } from 'vuex';
-
-@Component({
-  components: {
-    Message
-  }
-})
+@Component({})
 export default class Conversation extends Vue {
   @conversationStore.State conversation!: MessageObject[];
 
@@ -61,5 +61,56 @@ export default class Conversation extends Vue {
   overflow-x: hidden;
   max-height: 46vh;
   padding: 10px;
+}
+
+.chat_line {
+  display: flex;
+  height: fit-content;
+}
+
+.message_box {
+  width: fit-content;
+  max-width: 300px;
+  padding: 20px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  text-align: left;
+}
+
+.icon_container {
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  height: 40px;
+}
+
+.user {
+  flex-direction: row-reverse;
+
+  .icon {
+    margin-left: 10px;
+  }
+
+  .message_box {
+    color: white;
+    background-color: #187795;
+    // text-align: right;
+  }
+}
+
+.bot {
+  flex-direction: row;
+
+  .icon {
+    margin-right: 10px;
+  }
+
+  .message_box {
+    color: white;
+    background-color: #38686a;
+    // text-align: left;
+  }
 }
 </style>
