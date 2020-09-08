7f1414b0733b12bc733ab649122588e89fb2f44b
Pietro <p.crovari@gmail.com>
2020-09-08 17:58:51 +0200

    barchart plottato

--- frontend/src/components/toolbox/tools/data_visualization/charts/HistDistChart.vue
+++ frontend/src/components/toolbox/tools/data_visualization/charts/HistDistChart.vue
@@ -11,7 +11,7 @@ import { select, event } from 'd3-selection';
 import { scaleOrdinal, scaleLinear } from 'd3-scale';
 import { schemeCategory10 } from 'd3-scale-chromatic';
 import { axisBottom, axisLeft } from 'd3-axis';
-// @ts-ignore
+
 import { sum, histogram, min, max, bin, Bin, extent } from 'd3-array';
 import makeid from '@/utils/makeid';
 import { histDistExampleData } from '@/test/histDistExampleData';
@@ -72,6 +72,7 @@ export default class HistDistChart extends Vue {
   }
 
   // deatiled guide: https://codepen.io/thecraftycoderpdx/pen/jZyzKo
+  // guide no. 2: https://www.d3-graph-gallery.com/graph/shape.html#myline
   plotPie() {
     const svg = d3.select<Element, any>('#' + this.chartDivId);
 
@@ -88,22 +89,25 @@ export default class HistDistChart extends Vue {
 
     const bins = bin(this.chartData);
 
-    const maxBins = d3.max(bins, (d) => d.length);
+    const maxBins = d3.max(bins, (d: Array<number>) => d.length);
     const count = this.chartData.length;
 
     const y = d3
       .scaleLinear()
-      .domain([0, maxBins])
+      .domain([0, maxBins ? maxBins : 0])
       .nice()
       .range([this.height - this.margin, this.margin]);
 
+    const dataDomain = d3.extent(this.chartData)
+      ? d3.extent(this.chartData)
+      : [0, 1];
     const x = d3
       .scaleLinear()
-      .domain(d3.extent(this.chartData))
+      .domain(dataDomain)
       .nice()
       .range([this.margin, this.width - this.margin]);
 
-    const yAxis = (g) =>
+    const yAxis = (g: any) =>
       g
         .attr('transform', `translate(${this.margin},0)`)
         .call(d3.axisLeft(y).ticks(this.height / 40))
@@ -131,10 +135,10 @@ export default class HistDistChart extends Vue {
       .data(bins)
       .join('rect')
       .attr('fill', '#444')
-      .attr('x', (d: Bin<any, any>) => x(d.x0) + 1)
-      .attr('width', (d: Bin<any, any>) => Math.max(0, x(d.x1) - x(d.x0) - 1))
-      .attr('height', (d: Bin<any, any>) => y(0) - y(d.length))
-      .attr('y', (d: Bin<any, any>) => y(d.length));
+      .attr('x', (d: any) => x(d.x0) + 1)
+      .attr('width', (d: any) => Math.max(0, x(d.x1) - x(d.x0) - 1))
+      .attr('height', (d: any) => y(0) - y(d.length))
+      .attr('y', (d: any) => y(d.length));
 
     g.append('g')
       .attr('transform', 'translate(0,' + (this.height - this.margin) + ')')
