77211ad56f92ec64f4815723f4829bdaa741bbb5
Sara <sara.pido@mail.polimi.it>
2021-03-05 12:02:02 +0100

    - join pivots

--- backend/geco_conversation/pivot_actions/joinpivotaction.py
+++ backend/geco_conversation/pivot_actions/joinpivotaction.py
@@ -14,7 +14,7 @@ class JoinPivotAction(AbstractAction):
 	def logic(self,message, intent, entities):
 		from geco_conversation.data_analysis.data_analysis import DataAnalysis
 		if intent!='deny':
-			self.context.add_bot_msg(Utils.chat_message("Ok, I will join the two tables. It will take some times, at the end you will see the result on the right."))
+			#self.context.add_bot_msg(Utils.chat_message("Ok, I will join the two tables. It will take some times, at the end you will see the result on the right."))
 			print(len(self.context.workflow))
 			for i in range((len(self.context.workflow)-2), 0, -1):
 				print(i)
@@ -25,6 +25,8 @@ class JoinPivotAction(AbstractAction):
 						JoinPivot(self.context.workflow[-1], depends_on_2))#, joinby=self.status['joinby']))
 					break
 			self.context.workflow.run(self.context.workflow[-1])
+			self.context.add_bot_msgs([Utils.chat_message(
+				"Ok, I joined the two tables. The result is on the right."),Utils.table_viz('Join', self.context.workflow[-1].result, show=True)])
 
 			return DataAnalysis(self.context), True
 		else:
