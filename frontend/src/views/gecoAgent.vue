287991b83da68c1ecd22d015cef808fbf84b6038
Sara <sara.pido@mail.polimi.it>
2021-03-09 16:34:16 +0100

    - socketio frontend reconnection

--- frontend/src/views/gecoAgent.vue
+++ frontend/src/views/gecoAgent.vue
@@ -3,10 +3,10 @@
     <div class="title_bar">
       <h1>
         GeCo Agent
-        <font-awesome-icon class="icon" :icon="['fas', 'dna']" size="1x" />
+        <font-awesome-icon class="icon" :icon="['fas', 'dna']" size="1x"/>
       </h1>
       <div class="reset_button" @click="reset">
-        <font-awesome-icon class="icon" :icon="['fas', 'redo']" size="2x" />
+        <font-awesome-icon class="icon" :icon="['fas', 'redo']" size="2x"/>
         Reset
       </div>
     </div>
@@ -19,39 +19,46 @@
     <div class="grid_container_lower_row">
       <div class="prova pane_border box_pane">
         <div
-          v-for="item in stepList"
-          :key="item.name"
-          :class="['process_step_box', item.state]"
+            v-for="item in stepList"
+            :key="item.name"
+            :class="['process_step_box', item.state]"
         >
           <div>
             {{ item.name }}
           </div>
           <font-awesome-icon
-            class="download_icon"
-            :icon="['fas', 'download']"
-            size="1x"
-            @click="downloadFile(item.urlList, 'esempio.txt', 'text/plain')"
-            v-if="item.isDownloadButtonVisible"
+              class="download_icon"
+              :icon="['fas', 'download']"
+              size="1x"
+              @click="downloadFile(item.urlList, 'esempio.txt', 'text/plain')"
+              v-if="item.isDownloadButtonVisible"
           />
         </div>
       </div>
-      <div class="prova pane_border"><parameters-box></parameters-box></div>
+      <div class="prova pane_border">
+        <parameters-box></parameters-box>
+      </div>
     </div>
   </div>
 </template>
 
 <script src="/socket.io/socket.io.js"></script>
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator';
-import { namespace } from 'vuex-class';
+import {Component, Vue, Prop} from 'vue-property-decorator';
+import {namespace} from 'vuex-class';
 import io from 'socket.io-client';
 import Chat from './../components/chat/chat_interface.vue';
 import FunctionsArea from '../components/functionsArea/functionsArea.vue';
 import Toolbox from './../components/toolbox/toolbox_interface.vue';
 import ParametersBox from '@/components/ParametersBox.vue';
-import { conversation } from './../test/conversation';
+import {conversation} from './../test/conversation';
 
-const socket = io('/test', { path: '/geco_agent/socket.io' });
+const socket = io('/test', {
+  path: '/geco_agent/socket.io',
+  'reconnection': true,
+  'reconnectionDelay': 500,
+  'reconnectionAttempts': 10
+});
 // const socket = io('http://localhost:5980/test');
 const tools = namespace('tools');
 const gecoAgentStore = namespace('gecoAgent');
@@ -82,10 +89,10 @@ export default class GecoAgent extends Vue {
   @conversationStore.Mutation setSendButtonStatus!: (newValue: boolean) => void;
 
   @conversationStore.Mutation('parseJsonResponse') messageParser!: (
-    msg: MessageObject
+      msg: MessageObject
   ) => void;
   @parametersStore.Mutation('setParametersList') parameterParser!: (
-    payload: Parameter[]
+      payload: Parameter[]
   ) => void;
   @functionsAreaStore.Mutation('parseJsonResponse')
   availableChoicesParser!: (newChoices: AvailableChoiceJsonPayload) => void;
@@ -94,7 +101,7 @@ export default class GecoAgent extends Vue {
   @dataVizStore.Mutation setCharts!: (newCharts: DataSummaryPayload) => void;
   @dataVizStore.Action updateToolToShow!: (newTool: string) => void;
   @processStore.Mutation('parseJsonResponse') processParser!: (
-    newProcess: ProcessPanePayload
+      newProcess: ProcessPanePayload
   ) => void;
   @tableStore.Mutation setTable!: (tablePayload: TablePayload) => void;
   // @gecoAgentStore.Mutation updateLastMessageId!: (newValue: number) => void;
@@ -119,14 +126,15 @@ export default class GecoAgent extends Vue {
     // this.removeToolsFromPane(jsonPayload.remove);
     // this.addToolsToPane(jsonPayload.add);
   }
+
   conversation?: MessageObject[] = [];
   fieldList = [];
   isDownloadButtonVisible = false;
   linkList!: any;
   messageTypes = [
     // { typeName: 'query', nameSpace: 'gecoAgent/queryParameters' },
-    { typeName: 'message', nameSpace: 'gecoAgent/conversation' },
-    { typeName: 'select_annotations', nameSpace: 'tools' }
+    {typeName: 'message', nameSpace: 'gecoAgent/conversation'},
+    {typeName: 'select_annotations', nameSpace: 'tools'}
   ];
 
   jsonResponseParsingFunctions = {
@@ -153,7 +161,7 @@ export default class GecoAgent extends Vue {
   }
 
   created() {
-    socket.emit('ack', { message_id: this.lastMessageId });
+    socket.emit('ack', {message_id: this.lastMessageId});
     socket.on('json_response', (payload: any) => {
       if (payload.type) {
         console.log('server sent JSON_response', payload);
@@ -163,7 +171,7 @@ export default class GecoAgent extends Vue {
       }
     });
     socket.on('reconnect', () => {
-      socket.emit('ack', { message_id: this.lastMessageId });
+      socket.emit('ack', {message_id: this.lastMessageId});
       console.log('RECONNECT! Mando ack');
     });
   }
@@ -172,7 +180,7 @@ export default class GecoAgent extends Vue {
     if (this.message != '') {
       this.setSendButtonStatus(false);
       this.addUserMessage(this.message);
-      socket.emit('my_event', { data: this.message });
+      socket.emit('my_event', {data: this.message});
       this.editMessage('');
     }
   }
@@ -197,13 +205,13 @@ export default class GecoAgent extends Vue {
   pushBotMessage(msg: string) {
     if (msg != '') {
       // conversation.push({ sender: 'bot', text: msg });
-      this.messageParser({ sender: 'bot', text: msg });
+      this.messageParser({sender: 'bot', text: msg});
     }
   }
 
   downloadFile(content: any, fileName: string, contentType: string) {
     var a = document.createElement('a');
-    var file = new Blob([content], { type: contentType });
+    var file = new Blob([content], {type: contentType});
     a.href = URL.createObjectURL(file);
     a.download = fileName;
     a.click();
@@ -213,16 +221,16 @@ export default class GecoAgent extends Vue {
 
   reset() {
     if (
-      window.confirm('Do you want to lose all your progresses and start over?')
+        window.confirm('Do you want to lose all your progresses and start over?')
     ) {
       socket.emit('reset', {});
 
       this.editMessage('');
-      
-      socket.emit('ack', { message_id: -1 });
+
+      socket.emit('ack', {message_id: -1});
       this.resetProcess();
       this.resetToolPane();
-      
+
       this.updateFieldList([]);
       this.parameterParser([]);
 
@@ -235,6 +243,7 @@ export default class GecoAgent extends Vue {
 </script>
 <style scoped lang="scss">
 @import '../style/base.scss';
+
 .container {
   height: 95vh;
   overflow: hidden;
