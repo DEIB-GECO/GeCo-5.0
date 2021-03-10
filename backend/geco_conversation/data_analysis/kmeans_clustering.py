c812419568d81999b9f0c6032430a968b945f3ee
Sara <sara.pido@mail.polimi.it>
2021-03-10 15:39:15 +0100

    - add messages of kmeans in parameters list

--- backend/geco_conversation/data_analysis/kmeans_clustering.py
+++ backend/geco_conversation/data_analysis/kmeans_clustering.py
@@ -8,10 +8,9 @@ class KMeansClustering(AbstractAction):
 
     def on_enter(self):
         print('status', self.status)
-        list_param = {}
-        list_param['ds_name']=self.context.workflow[-1].result.name
-        list_param['data_analysis_op'] = 'KMeans Clustering'
-        self.context.add_bot_msg(Utils.param_list(list_param))
+        self.context.payload.insert('ds_name', self.context.workflow[-1].result.name)
+        self.context.payload.insert('data_analysis_op', 'KMeans Clustering')
+        self.context.add_bot_msg(Utils.param_list({k:v[0] for k,v in self.status.items()}))
         self.context.add_bot_msg(
             Utils.chat_message("Do you already know how many clusters to create? If so, tell me the number, otherwise I will apply parameter tuning."))
 
@@ -23,6 +22,7 @@ class KMeansClustering(AbstractAction):
 
         if intent != 'deny':
             n_clust = int(''.join(filter(str.isdigit, message)))
+            self.context.payload.insert('n_clusters', n_clust)
             # self.context.add_bot_msg(
             #   Utils.chat_message("Ok, I will perform K-Means clustering using {}.".format(n_clust)))
             self.context.workflow.add(KMeans(self.context.workflow[-1], clusters=n_clust))
@@ -34,6 +34,7 @@ class KMeansClustering(AbstractAction):
                 Utils.scatter(self.context.workflow[-1].result.x, self.context.workflow[-1].result.y,
                               self.context.workflow[-1].result.labels, self.context.workflow[-1].result.u_labels))
             self.context.add_bot_msg(Utils.chat_message(f"Ok, I did K-Means clustering using {n_clust}."))
+            self.context.add_bot_msg(Utils.param_list({k: v[0] for k, v in self.status.items()}))
             self.context.add_bot_msg(Utils.chat_message("Do you want to do again the K-Means Clustering?"))
             return YesNoAction(self.context, KMeansClustering(self.context), ByeAction(self.context)), False
 
@@ -51,21 +52,24 @@ class NumClusters(AbstractAction):
 
     def logic(self, message, intent, entities):
         print(self.status)
-        if 'min' not in self.status:
+        if 'min_n_clusters' not in self.status:
             min = int(''.join(filter(str.isdigit, message)))
-            self.context.payload.insert('min', min)
+            self.context.payload.insert('min_n_clusters', min)
+            self.context.add_bot_msg(Utils.param_list({k: v[0] for k, v in self.status.items()}))
             print('status after min', self.status)
             self.context.add_bot_msg(Utils.chat_message(messages.max_n_clust))
             return NumClusters(self.context), False
         else:
             max = int(''.join(filter(str.isdigit, message)))
-            self.context.payload.insert('max', max)
+            self.context.payload.insert('max_n_clusters', max)
             self.context.add_bot_msg(Utils.chat_message(messages.analysis_done))
-            self.context.workflow.add(KMeans(self.context.workflow[-1], tuning=True, min=self.status['min'][0], max=self.status['max'][0]))
+            self.context.workflow.add(KMeans(self.context.workflow[-1], tuning=True, min=self.status['min_n_clusters'][0], max=self.status['max_n_clusters'][0]))
             self.context.workflow.add(PCA(self.context.workflow[-1], 2))
             self.context.workflow.add(Scatter(self.context.workflow[-1], self.context.workflow[-2]))
 
             self.context.workflow.run(self.context.workflow[-1],self.context.session_id)
+            self.context.payload.insert('used_n_clusters', len(self.context.workflow[-1].result.u_labels))
+            self.context.add_bot_msg(Utils.param_list({k: v[0] for k, v in self.status.items()}))
             self.context.add_bot_msg(
                 Utils.scatter(self.context.workflow[-1].result.x, self.context.workflow[-1].result.y,
                               self.context.workflow[-1].result.labels, self.context.workflow[-1].result.u_labels))
